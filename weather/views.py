71ffdf206d45fe16c0f5d2174e41036b85093e8d
Nikita Saganovich <nikis2345@mail.ru>
2023-05-11 01:32:58 +0300

    Вывел информацию о погоде в Лондоне

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,22 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = '9549f93f8304db2f6cd0cd9a4fd458e0'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    city = 'London'
+    res = requests.get(url.format(city)).json()
+
+    city_info ={
+        'city':city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)
 
 def about(request):
     return render(request, 'weather/about.html')
